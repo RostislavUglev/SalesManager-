4d65f074b90dfb172f31f9f3648d19f51fbd6496
Rostislav Uglev <urg@mail.ru>
2022-07-28 19:05:34 +1000

    Изменение int на long

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int [] sales = {70, 35, 10, 60};
+        long [] sales = {70, 35, 10, 60};
         SalesManager salesManager = new SalesManager (sales);
-        int maxSales = salesManager.max();
+        long maxSales = salesManager.max();
         System.out.println (maxSales);
     }
 
